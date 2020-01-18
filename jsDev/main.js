adb4236f63d72aa12bc7f549623c7b1464ce7c32
Jose Guerra <jose@gcostudios.com>
2020-01-18 16:54:09 +0000

    Initial commit

---
+++ jsDev/main.js
@@ -0,0 +1,27 @@
+jQuery(function($) {
+
+  'use strict';
+
+  var ENHANCEMENTS;
+
+  // ================================
+  // Enhancements
+  // ================================
+
+    ENHANCEMENTS = {
+
+    init: function() {
+      this.customScript();
+    },
+
+    // Navigation auto-close on click
+    // ==============================================
+    customScript: function() {
+      // your script here
+    }
+
+  };
+  
+  ENHANCEMENTS.init();
+
+}); //jQuery
