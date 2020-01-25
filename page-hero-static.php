a81eece58c1da6eef27490d95c2138a734dbfcfb
Jose Guerra <jose@gcostudios.com>
2020-01-25 23:34:26 +0000

    updated markup and php

--- page-hero-static.php
+++ page-hero-static.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template Name: Hero Slideshow
+ * Template Name: Hero Single Image
  * description: Page template with optional Hero
  */
 
@@ -10,21 +10,60 @@ get_header();
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
+    <?php
+      $backgroundImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );
+      $get_description = get_post(get_post_thumbnail_id())->post_excerpt;
 
+      while ( have_posts() ) :
+        the_post();
+    ?>
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+    <div class="hero-treatment" style="background-image: url('<?php echo $backgroundImg[0]; ?>');">
+        <?php
+          if(!empty($get_description)){//If description is not empty show the div
+            echo '<div class="featured-caption"><p>' . $get_description . '</p></div>';
+          }
+        ?>
+    </div>
 
-			get_template_part( 'template-parts/content', 'page' );
+    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+      <div class="entry-content">
+        <?php
+        the_content();
 
-		endwhile; // End of the loop.
-		?>
+        wp_link_pages( array(
+          'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'webuyproperty' ),
+          'after'  => '</div>',
+        ) );
+        ?>
+      </div><!-- .entry-content -->
+
+      <?php if ( get_edit_post_link() ) : ?>
+        <footer class="entry-footer">
+          <?php
+          edit_post_link(
+            sprintf(
+              wp_kses(
+                /* translators: %s: Name of current post. Only visible to screen readers */
+                __( 'Edit <span class="screen-reader-text">%s</span>', 'webuyproperty' ),
+                array(
+                  'span' => array(
+                    'class' => array(),
+                  ),
+                )
+              ),
+              get_the_title()
+            ),
+            '<span class="edit-link">',
+            '</span>'
+          );
+          ?>
+        </footer><!-- .entry-footer -->
+      <?php endif; ?>
+    </article><!-- #post-<?php the_ID(); ?> -->
+
+    <?php endwhile; // End of the loop. ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
