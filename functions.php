2f11b92507b76bed73cb0f198d34292a665e20c6
Jose Guerra <jose@gcostudios.com>
2020-02-05 23:02:54 +0000

    registered new sidebars

--- functions.php
+++ functions.php
@@ -112,6 +112,46 @@ function webuyproperty_widgets_init() {
 		'after_widget'  => '</section>',
 		'before_title'  => '<h2 class="widget-title">',
 		'after_title'   => '</h2>',
+  ) );
+  
+  register_sidebar( array(
+		'name'          => esc_html__( 'Footer widget 1', 'webuyproperty' ),
+    'id'            => 'footer-widget-1',
+		'description'   => esc_html__( 'Add widgets here.', 'webuyproperty' ),
+		'before_widget' => '<section id="%1$s" class="widget footer-widget-1 %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h3 class="widget-title">',
+		'after_title'   => '</h3>',
+  ) );
+  
+  register_sidebar( array(
+		'name'          => esc_html__( 'Footer widget 2', 'webuyproperty' ),
+		'id'            => 'footer-widget-2',
+		'description'   => esc_html__( 'Add widgets here.', 'webuyproperty' ),
+		'before_widget' => '<section id="%1$s" class="widget footer-widget-2 %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h3 class="widget-title">',
+		'after_title'   => '</h3>',
+  ) );
+  
+  register_sidebar( array(
+		'name'          => esc_html__( 'Footer widget 3', 'webuyproperty' ),
+		'id'            => 'footer-widget-3',
+		'description'   => esc_html__( 'Add widgets here.', 'webuyproperty' ),
+		'before_widget' => '<section id="%1$s" class="widget footer-widget-3 %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h3 class="widget-title">',
+		'after_title'   => '</h3>',
+  ) );
+  
+  register_sidebar( array(
+		'name'          => esc_html__( 'Footer widget 4', 'webuyproperty' ),
+		'id'            => 'footer-widget-4',
+		'description'   => esc_html__( 'Add widgets here.', 'webuyproperty' ),
+		'before_widget' => '<section id="%1$s" class="widget footer-widget-4 %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h3 class="widget-title">',
+		'after_title'   => '</h3>',
 	) );
 }
 add_action( 'widgets_init', 'webuyproperty_widgets_init' );
