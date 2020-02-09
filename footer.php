bcbef8368262015b4cfe0d283806b32fb30cc374
Jose Guerra <jose@gcostudios.com>
2020-02-09 22:30:05 +0000

    added menu

--- footer.php
+++ footer.php
@@ -13,9 +13,17 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="footer-top-row">
-      <div class="">
+      <div class="footer-logo">
         <img src="<?php echo get_template_directory_uri(); ?>/images/footer-logo.png" />
       </div>
+      <nav class="footer-links">
+			<?php
+			wp_nav_menu( array(
+				'theme_location' => 'menu-2',
+				'menu_id'        => 'secondary-menu',
+			) );
+			?>
+    </nav><!-- #site-navigation -->
     </div><!-- .footer-top-row -->
     <div class="footer-bottom-row">
       <?php if( dynamic_sidebar( 'footer-widget-1' ) ) : endif ?>
