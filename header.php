d57a0228a1573e1cceb6b4ce75215efdd32a303d
Jose Guerra <jose@gcostudios.com>
2020-02-22 21:04:31 +0000

    updated markup

--- header.php
+++ header.php
@@ -44,8 +44,9 @@
 			<?php endif; ?>
 		</div><!-- .site-branding -->
 
+    <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'webuyproperty' ); ?></button>
+
 		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'webuyproperty' ); ?></button>
 			<?php
 			wp_nav_menu( array(
 				'theme_location' => 'menu-1',
